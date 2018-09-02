940b0f2e793f8dad90c854c1dfc6b6bee6095826
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 20:52:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
 "SELECT authors.name, subgenres.name
 FROM authors
 JOIN series ON authors.id = series.author_id
-JOIN subgenres ON "
+JOIN subgenres ON series.subgenre_id = subgenres.id"
 
 SELECT authors.name, subgenres.name
   FROM authors
