fc7fd0b3ad4ffd4af80d2b1594ed98859b19b9c2
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 20:49:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 def select_name_and_series_subgenres_of_authors
 "SELECT authors.name, subgenres.name
 FROM authors
-JOIN subgenres, ON authors.name = subgenres.name"
+JOIN subgenres ON authors.name = subgenres.name"
 end
 
 def select_series_title_with_most_human_characters
