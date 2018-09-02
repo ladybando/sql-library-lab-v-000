bfb71276194482954c40bf00889c90713a79b4c3
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 19:55:09 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,3 +3,39 @@ CREATE TABLE cats (
     name TEXT, 
     age INTEGER
 );
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+CREATE TABLE sub-genres (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
