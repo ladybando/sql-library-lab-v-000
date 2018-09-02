838ba869ccce8727a704b7c38b35e50fa6b6e674
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 20:28:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  ""
+"SELECT column_name(s)
+FROM first_table
+JOIN second_table ON first_table.column_name = second_table.column_name
+GROUP BY table_name.column_name, table_name
+ORDER BY aggregate function (i.e. SUM, AVERAGE)(table_name.column_name), column_name;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
