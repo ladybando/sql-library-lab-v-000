4c26ad9725dfb398bf263fc6e05b5d35d5dc0d25
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 20:40:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species
-FROM characters
-column_name ASC|DESC, column_name ASC|DESC"
+  "SELECT bears.color, COUNT(bears.color) 
+  FROM bears GROUP BY bears.color
+  ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
