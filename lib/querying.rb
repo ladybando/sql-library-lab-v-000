3df486481561e831984c7f725f661d02d52c857e
Ayana Bando <ayanaflax@yahoo.com>
2018-09-02 20:41:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT bears.color, COUNT(bears.color) 
+  "SELECT species, COUNT(character.species) 
   FROM bears GROUP BY bears.color
   ORDER BY COUNT(*) DESC LIMIT 1;"
 end
